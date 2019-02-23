b26ea6b9b4446bdd916483ec9cb2d423c50e2072
Marcel Savegnago <marcel.savegnago@gmail.com>
2019-02-23 03:11:43 -0300

    [ADD] helpdesk modules

---
+++ helpdesk_project_ext/models/helpdesk_team.py
@@ -0,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class HelpdeskTeam(models.Model):
+
+    _inherit = 'helpdesk.team'
+
+    project_id = fields.Many2one('project.project', string='Project')
+    need_project = fields.Boolean(string='Need Project',
+                                  compute="_compute_need_project")
+
+    @api.multi
+    def _compute_need_project(self):
+        config = self.env['res.config.settings'].search(
+            [], limit=1, order='id desc')
+        for team in self:
+            team.need_project = False
+            if config.use_project:
+                team.need_project = True
