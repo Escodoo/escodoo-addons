a672b1a6b53159338d3bdffcb9a9f9ac2bb9201d
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-01-08 13:58:29 -0300

    [IMP] escodoo_crm_custom: black, isort, prettier

--- escodoo_crm_custom/__manifest__.py
+++ escodoo_crm_custom/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Escodoo Crm Custom',
-    'summary': """
+    "name": "Escodoo Crm Custom",
+    "summary": """
         Escodoo CRM Custom""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo,Odoo Community Association (OCA)',
-    'website': 'https://github.com/escodoo/escodoo-addons',
-    'depends': [
-        'crm',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/Escodoo/escodoo-addons",
+    "depends": [
+        "crm",
     ],
-    'data': [
-        'views/crm_lead.xml',
+    "data": [
+        "views/crm_lead.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
