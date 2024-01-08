3822736675b05383d75abb6eed80a4f111ad3c6c
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-01-08 13:59:08 -0300

    [MIG] escodoo_crm_custom: Migration to 14.0

--- escodoo_crm_custom/models/crm_lead.py
+++ escodoo_crm_custom/models/crm_lead.py
@@ -8,7 +8,32 @@ class CrmLead(models.Model):
 
     _inherit = "crm.lead"
 
-    escodoo_company_size = fields.Integer(string="Company Size")
+    escodoo_primary_interest = fields.Selection(
+        [
+            ("seeking_erp", "My company is seeking an ERP"),
+            ("using_odoo", "My company uses Odoo and needs services or improvements"),
+            (
+                "external_consultant",
+                "I am an external consultant and want to recommend Odoo",
+            ),
+            ("partnership_opportunity", "Partnership Opportunity"),
+        ],
+        string="Primary Interest",
+    )
+    escodoo_has_management_system = fields.Boolean(string="Has Management System?")
+    escodoo_management_system_name = fields.Char(string="Management System Name")
+    escodoo_company_size = fields.Selection(
+        [
+            ("micro", "Less than 5 Employees"),
+            ("small", "5-20 Employees"),
+            ("medium_small", "20-50 Employees"),
+            ("medium", "50-100 Employees"),
+            ("medium_large", "100-250 Employees"),
+            ("large", "More than 250 Employees"),
+        ],
+        string="Company Size",
+    )
+    field_name = fields.Selection([("key", "value")], string="field_name")
     escodoo_annual_revenue = fields.Monetary(
         "Annual Revenue", currency_field="company_currency", track_visibility="always"
     )
@@ -25,5 +50,5 @@ class CrmLead(models.Model):
     )
     escodoo_project_release_date = fields.Date("Release Date")
     escodoo_project_description = fields.Html(string="General Description")
-    escodoo_project_pain_primary = fields.Html(string="Primary Pains")
+    escodoo_project_primary_pain = fields.Html(string="Primary Pains")
     escodoo_project_integration = fields.Html(string="Integrations")
