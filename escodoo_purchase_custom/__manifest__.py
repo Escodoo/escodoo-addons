5f8ede4b9c3700b879c8fd3015931578a9c204bd
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-01-30 12:48:32 -0300

    [ADD] escodoo_purchase_custom: add new module

---
+++ escodoo_purchase_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Escodoo Purchase Custom",
+    "summary": """
+        Escodoo Purchase Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/escodoo-addons",
+    "depends": ["l10n_br_purchase"],
+    "data": [
+        "views/purchase_views.xml",
+    ],
+}
---
+++ escodoo_purchase_custom/models/purchase_order.py
@@ -0,0 +1,9 @@
+# Copyright 2024 - TODAY, Kaynnan Lemes <kaynnan.lemes@escodooo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = "purchase.order"
