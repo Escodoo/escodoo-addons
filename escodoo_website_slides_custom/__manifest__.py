111b0ba56e762936aec06438f57807739c0d008c
Kaynnan Lemes <kaynnanx1@gmail.com>
2025-07-16 09:05:52 -0300

    [MIG] escodoo_website_slides_custom: Migration to 16.0

--- escodoo_website_slides_custom/__manifest__.py
+++ escodoo_website_slides_custom/__manifest__.py
@@ -5,14 +5,17 @@
     "name": "Escodoo Website Slides Custom",
     "summary": """
         Escodoo Website Slides Custom""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/escodoo-addons",
     "depends": ["website_slides"],
     "data": [
-        "views/assets.xml",
         "views/website_slides_templates_homepage.xml",
     ],
-    "demo": [],
+    "assets": {
+        "web.assets_frontend": [
+            "escodoo_website_slides_custom/static/src/scss/website_slides.scss",
+        ],
+    },
 }
