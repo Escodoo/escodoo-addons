04194619b55844b8425d74494fa3febfcff11c27
Kaynnan Lemes <kaynnanx1@gmail.com>
2026-03-01 11:24:58 -0300

    [MIG] escodoo_helpdesk_mgmt_custom: Migration to 18.0

--- escodoo_helpdesk_mgmt_custom/__manifest__.py
+++ escodoo_helpdesk_mgmt_custom/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Escodoo Helpdesk Mgmt Custom",
     "summary": """Escodoo Helpdesk Custom""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/escodoo-addons",
